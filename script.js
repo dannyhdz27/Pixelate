7199dfa27726243b18d2f6ba353a0c480781afa3
Daniel H <dannyhdz27@gmail.com>
2023-03-06 10:38:34 -0600

    worked on DOM row function

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Your code here
+// document.createElement('ul')
+// parentElement.appendChild('li')
+const table = document.getElementsByTagName("table")[0];
+
+function makeRow() {
+  const row = document.createElement("tr");
+  for (let i = 0; i < 20; i++) {
+    const td = document.createElement("td");
+    row.appendChild(td);
+  }
+  table.appendChild(row);
+  //   parentElement.appendChild("td");
+}
+makeRow();
+makeRow();
+makeRow();
